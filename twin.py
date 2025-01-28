d165ac4df1cdfbcf452789a949167b22e9f0c17d
Tobias3sprong <tobias@tobiasdriesprong.nl>
2025-01-28 10:53:59 +0100

    Update twin.py

--- twin.py
+++ twin.py
@@ -132,13 +132,13 @@ def publish(client):
     global deviceID, IMSI, WanIP
     try:
         # Resultaten van de drie requests
-        response2 = modbusclient.read_holding_registers(12, count=125, slave=3)
-        response3 = modbusclient.read_holding_registers(162, count=6, slave=3)
+        #response2 = modbusclient.read_holding_registers(12, count=125, slave=3)
+        #response3 = modbusclient.read_holding_registers(162, count=6, slave=3)
         response1 = modbusclient.read_holding_registers(3000, count=16, slave=3)
         
 
         # Registers samenvoegen
-        combined_registers = response1.registers + response2.registers + response3.registers
+        combined_registers = response1.registers# + response2.registers + response3.registers
 
         # Gecombineerde registers printen
         print(combined_registers)
