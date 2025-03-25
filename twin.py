9831391390f9b19c99df29d93e67849b4716d339
Tobias3sprong <tobias@tobiasdriesprong.nl>
2025-03-25 09:29:40 +0100

    Update twin.py

--- twin.py
+++ twin.py
@@ -28,7 +28,7 @@ def on_disconnect(client, userdata, rc):
 # MODBUS
 # Set up modbus RTU
 comapA = ModbusSerialClient(
-    port='/dev/ttyUSB6',
+    port='/dev/ttyUSB4',
     stopbits=1,
     bytesize=8,
     parity='N',
@@ -37,7 +37,7 @@ comapA = ModbusSerialClient(
 )
 
 comapB = ModbusSerialClient(
-    port='/dev/ttyUSB7',
+    port='/dev/ttyUSB5',
     stopbits=1,
     bytesize=8,
     parity='N',
