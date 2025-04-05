9f8efcfb93caf8c9e9444cf3f8a6320f19df61ff
Tobias3sprong <tobias@tobiasdriesprong.nl>
2025-04-05 20:11:58 +0200

    Update twin.py

--- twin.py
+++ twin.py
@@ -68,7 +68,7 @@ teltonika = ModbusTcpClient(
 
 # Set up modbus RTU for powerlogger
 powerlogger = ModbusSerialClient(
-    port='usb_serial_dbe17d6e',
+    port='/dev/usb_serial_dbe17d6e',
     stopbits=1,
     bytesize=8,
     parity='N',
